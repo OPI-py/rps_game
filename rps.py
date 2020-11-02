baaf321b48079001a0ac26b91f9e978e05048262
Oleksandr <70897912+OPI-py@users.noreply.github.com>
2020-11-02 22:38:46 +0200

    Update rps.py

--- rps.py
+++ rps.py
@@ -2,22 +2,22 @@ from tkinter import Tk, Label, Button, messagebox
 import random
 
 win = Tk()
-win.title("Камінь, Ножиці, Бумага")
+win.title("Rock, Paper, Scissors")
 win.resizable(0, 0)
 win.iconbitmap('favicon.ico')
 
-result = Label(win, text="Виберіть штучку", font=("Arial Black", 30),
+result = Label(win, text="Choose option", font=("Arial Black", 30),
                width=20, bg="#ffdba1", pady=20)
 result.grid(row=0, column=0, columnspan=4, sticky="nsew")
 
 def message1():
-    messagebox.showinfo("Вітаємо", "    Ви виграли ;)    ")
+    messagebox.showinfo("Grats", "    You win ;)    ")
     
 def message2():
-    messagebox.showinfo("Хехе", "       Нічия        ")
+    messagebox.showinfo("Hehe", "       Draw        ")
     
 def message3():
-    messagebox.showinfo("Жиш", "    Не повезло :)   ")
+    messagebox.showinfo("Hmm", "    Bad Luck :)   ")
 
 def scissors():
     ai_choice = random.randint(1, 3)
@@ -30,7 +30,7 @@ def scissors():
         return message1()
     else:
         return "Error"
-b_scissors = Button(win, text="Ножиці", bg="#ff7171", width=20,
+b_scissors = Button(win, text="Scissors", bg="#ff7171", width=20,
                     font=("Arial Black", 20), command=scissors)
 b_scissors.grid(row=1, column=0, columnspan=1, sticky="nsew")
 
@@ -46,7 +46,7 @@ def stone():
     else:
         return "Error"
 
-b_stone = Button(win, text="Камінь", bg="#3d3d3b", fg="White",
+b_stone = Button(win, text="Stone", bg="#3d3d3b", fg="White",
                  width=20, font=("Arial Black", 20), command=stone)
 b_stone.grid(row=1, column=1, columnspan=1, sticky="nsew")
 
@@ -62,11 +62,11 @@ def paper():
     else:
         return "Error"
 
-b_paper = Button(win, text="Бумага", bg="#ffffc9",
+b_paper = Button(win, text="Paper", bg="#ffffc9",
                  width=20, font=("Arial Black", 20), command=paper)
 b_paper.grid(row=1, column=3, columnspan=1, sticky="nsew")
 
-ex = Button(win, text="Вихід", width=20, font=("Arial Black", 20),
+ex = Button(win, text="Exit", width=20, font=("Arial Black", 20),
                                                 command=win.destroy)
 ex.grid(row=2, columnspan=4, sticky="nsew")
 
